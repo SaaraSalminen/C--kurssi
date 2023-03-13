bf08aa075f4dba54dc3f02d77aeb86c3a502cb0c
Joonas Kalliolevo <joonas.kalliolevo@gmail.com>
2023-03-13 16:30:28 +0200

    Added Cart class and interaction with user

---
+++ Cart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Cart
+{
+    public List<Product> items;
+
+    public Cart()
+    {
+        items = new List<Product>();
+    }
+
+    public void AddToCart(Product product)
+    {
+        items.Add(product);
+    }
+
+    public void RemoveFromCart(int index)
+    {
+        items.Remove(items[index]);
+    }
+
+    public void DisplayCart()
+    {
+        Console.WriteLine("---------------");
+        for (int i = 0; i < items.Count; i++)
+        {
+            Console.WriteLine("[" + i + "] " + items[i].Name);
+        }
+        Console.WriteLine("---------------");
+    }
+
+    public double CalculateTotal()
+    {
+        double total = 0;
+        foreach (Product item in items)
+        {
+            total += item.Price;
+        }
+        return total;
+    }
+}
